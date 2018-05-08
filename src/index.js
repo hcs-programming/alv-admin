4ee3a28dd63eaa71cae5f3e8f5c268265a26ba1a
KGrimm28 <31281460+KGrimm28@users.noreply.github.com>
2018-05-08 10:00:19 -0400

    boi swag

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
-import { Segment, Container, Header, Icon } from "semantic-ui-react"
+import { Container, Accordion, Icon } from "semantic-ui-react"
 
 class App extends React.Component {
-  state = {
-    
-  }
+  state = { activeIndex: 0 }
   render() {
+    const { activeIndex } = this.state
     return (
       <Container className="App">
-        
+        <Accordion styled>
+          <Accordion.Title active={activeIndex === 0}>
+            <Icon name="dropdown" />Attendance
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 0}>
+            LeBron James, Kobe Bryant
+          </Accordion.Content>
+          <Accordion.Title active={activeIndex === 1}>
+            <Icon name="dropdown" />Lunch
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 1}>
+            LeBron James, Kobe Bryant
+          </Accordion.Content>
+          <Accordion.Title active={activeIndex === 2}>
+            <Icon name="dropdown" />Violations
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 2}>
+            LeBron James, Kobe Bryant
+          </Accordion.Content>
+        </Accordion>
       </Container>
     )
   }
