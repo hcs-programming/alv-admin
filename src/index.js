61a829fe18ea75eeb4b69aeaf82922b72e20c6bd
Samdemanyman <31315687+Samdemanyman@users.noreply.github.com>
2018-05-09 09:57:43 -0400

    Accordion works

--- src/index.js
+++ src/index.js
@@ -1,32 +1,48 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
-import { Container, Accordion, Icon } from "semantic-ui-react"
+import { Container, Accordion, Icon, Segment } from "semantic-ui-react"
 
 class App extends React.Component {
-  state = { activeIndex: 0 }
+  state = { activeIndex: 1 }
+  changeActiveIndex = index => {
+    this.setState({ activeIndex: index })
+  }
   render() {
     const { activeIndex } = this.state
     return (
       <Container className="App">
-        <Accordion styled>
-          <Accordion.Title active={activeIndex === 0}>
+        <Accordion styled fluid>
+          <Accordion.Title
+            active={activeIndex === 0}
+            onClick={() => this.changeActiveIndex(0)}
+          >
             <Icon name="dropdown" />Attendance
           </Accordion.Title>
           <Accordion.Content active={activeIndex === 0}>
-            LeBron James, Kobe Bryant
+            <Segment.Group>
+              <Segment>muss brown</Segment>
+              <Segment>nathan</Segment>
+              <Segment>niwe</Segment>
+            </Segment.Group>
           </Accordion.Content>
-          <Accordion.Title active={activeIndex === 1}>
+          <Accordion.Title
+            active={activeIndex === 1}
+            onClick={() => this.changeActiveIndex(1)}
+          >
             <Icon name="dropdown" />Lunch
           </Accordion.Title>
           <Accordion.Content active={activeIndex === 1}>
-            LeBron James, Kobe Bryant
+            , Kobe Bryant
           </Accordion.Content>
-          <Accordion.Title active={activeIndex === 2}>
+          <Accordion.Title
+            active={activeIndex === 2}
+            onClick={() => this.changeActiveIndex(2)}
+          >
             <Icon name="dropdown" />Violations
           </Accordion.Title>
           <Accordion.Content active={activeIndex === 2}>
-            LeBron James, Kobe Bryant
+            , Kobe Bryant
           </Accordion.Content>
         </Accordion>
       </Container>
